28f11b575c64b13e99572cb23862eb837bead008
Kat <catherineitj28@gmail.com>
2023-02-22 19:46:50 +0300

    fix: fix modal layout

--- src/components/ConfirmModalComponent.vue
+++ src/components/ConfirmModalComponent.vue
@@ -9,34 +9,28 @@ const confirm = () => {
 </script>
 
 <template>
-  <div
-    class="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 md:inset-0 h-modal md:h-full justify-center items-center bg-black bg-opacity-60"
-    aria-hidden="true"
-  >
-    <div class="modal">
-      <div class="absolute p-4 w-full max-w-md h-full md:h-auto">
-        <div class="relative bg-white rounded-lg shadow">
-          <div class="p-6 text-center">
-            <h3 class="mb-5 text-lg font-normal text-gray-500">
-              <slot v-if="modalData == null">Отменить редактирование?</slot>
-              <slot v-if="modalData == 'note'">Удалить заметку?</slot>
-            </h3>
-            <button
-              type="button"
-              class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2"
-              @click="confirm"
-            >
-              Да
-            </button>
-            <button
-              type="button"
-              class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10"
-              @click="$emit('close')"
-            >
-              Нет
-            </button>
-          </div>
-        </div>
+  <div class="modal-overlay" aria-hidden="true"></div>
+  <div class="modal">
+    <div class="relative bg-white rounded-lg shadow">
+      <div class="p-6 text-center">
+        <h3 class="mb-5 text-lg font-normal text-gray-500">
+          <slot v-if="modalData == null">Отменить редактирование?</slot>
+          <slot v-if="modalData == 'note'">Удалить заметку?</slot>
+        </h3>
+        <button
+          type="button"
+          class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2"
+          @click="confirm"
+        >
+          Да
+        </button>
+        <button
+          type="button"
+          class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10"
+          @click="$emit('close')"
+        >
+          Нет
+        </button>
       </div>
     </div>
   </div>
@@ -45,10 +39,22 @@ const confirm = () => {
 <style scoped>
 .modal {
   position: fixed;
-  z-index: 999;
-  top: 20%;
+  z-index: 1000;
+  top: 50%;
   left: 50%;
-  width: 400px;
-  margin-left: -150px;
+  transform: translate(-50%, -50%);
+  max-height: 100%;
+  width: 50%;
+}
+.modal-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.7);
+  width: 100vw;
+  height: 100vh;
+  z-index: 900;
+  overflow-x: hidden;
+  overflow-y: hidden;
 }
 </style>
