ab8648070a983f953812a422bb6c0960831d8f1d
kat <catherineitj28@gmail.com>
2024-11-14 22:55:50 +0300

    Refactor HomeView component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,26 +8,27 @@ const store = useNoteStore();
 
 onMounted(() => {
   store.loadNotes();
-  document.title = 'Your Notes';
+  document.title = 'Since Notion is gone...';
 });
 
 onUnmounted(() => store.saveNotes());
 
 const noteName = ref('');
 const listOfTodos = ref('');
-// TODO: add error handling
 
 const addNewNote = () => {
-  if (noteName.value.trim().length && listOfTodos.value.trim().length) {
-    const noteToAdd = {
-      id: uuid.v1(),
-      name: noteName.value,
-      todos: listOfTodos.value
-        .split(', ')
-        .map((todo) => ({ name: todo, completed: false })),
-    };
-    store.addNote(noteToAdd);
-  }
+  const name = noteName.value.trim();
+  const todos = listOfTodos.value.trim();
+
+  if (!name || !todos) return;
+
+  const noteToAdd = {
+    id: uuid.v1(),
+    name,
+    todos: todos.split(', ').map((todo) => ({ name: todo, completed: false })),
+  };
+
+  store.addNote(noteToAdd);
 
   noteName.value = '';
   listOfTodos.value = '';
@@ -46,8 +47,9 @@ const addNewNote = () => {
         id="note"
         type="text"
         v-model="noteName"
-        placeholder="Note"
+        placeholder="Add a new note"
       />
+
       <input
         required
         class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500 mr-8 mt-3 sm:mt-0"
@@ -55,16 +57,18 @@ const addNewNote = () => {
         type="text"
         v-model="listOfTodos"
         @keyup.enter="addNewNote()"
-        placeholder="Tasks"
+        placeholder="Write down some fancy tasks"
       />
+
       <button
         class="flex-shrink-0 bg-blue-500 hover:bg-blue-700 border-blue-500 hover:border-blue-700 text-sm border-4 text-white py-1 px-2 rounded mt-3 sm:mt-0"
-        type="button"
+        type="submit"
         @click="addNewNote()"
       >
         Submit
       </button>
     </form>
+
     <NoteListComponent />
   </div>
 </template>
